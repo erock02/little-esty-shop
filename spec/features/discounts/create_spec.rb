2b1837cd834b45ff2f3a15d9e09e3ccc91de997a
Eric Espindola <eric_e_2012@hotmail.com>
2022-06-13 07:51:40 -0700

    create tests for discount create feature. Re-organized folders

---
+++ spec/features/discounts/create_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'discounts create page', :vcr do
+  it 'can create a new discount given valid data' do
+    merchant1 = Merchant.create!(name: "Schroeder-Jerde")
+    visit "/merchants/#{merchant1.id}/discounts"
+    expect(page).to_not have_content("Buy 30 items, get 40% off")
+
+    click_link("Create New Discount")
+
+    expect(current_path).to eq(new_merchant_discount_path)
+    fill_in 'discount[discount]', with: "40"
+    fill_in 'discount[threshold]', with: "30"
+    click_button
+    expect(current_path).to eq("/merchants/#{merchant1.id}/discounts")
+    expect(page).to have_content("Buy 30 items, get 40% off")
+  end
+end
