36bff2a8b812eb182f73e07dcadfbbcba98ddde5
Eric Espindola <eric_e_2012@hotmail.com>
2022-06-13 16:47:01 -0700

    add create discount feature

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -4,4 +4,18 @@ class DiscountsController < ApplicationController
   def index
     @discounts = @merchant.discounts
   end
+
+  def new
+
+  end
+
+  def create
+    @merchant.discounts.create!(discount_params)
+    redirect_to merchant_discounts_path(@merchant)
+  end
+
+  private
+    def discount_params
+      params.permit(:discount, :threshold, :merchant_id)
+    end
 end
