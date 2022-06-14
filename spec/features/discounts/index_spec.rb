aff2f142280aa3ee161eb118c47faead94aa1cee
Eric Espindola <eric_e_2012@hotmail.com>
2022-06-13 17:24:49 -0700

    create delete discount feature test for merchant index page

--- spec/features/discounts/index_spec.rb
+++ spec/features/discounts/index_spec.rb
@@ -18,4 +18,24 @@ RSpec.describe 'merchant discount index', :vcr do
     expect(page).to have_content("Buy 5 items, get 5% off")
     expect(page).to_not have_content("Buy 10 items, get 20% off")
   end
+
+  it 'can delete a discount' do
+    merchant1 = Merchant.create!(name: "Schroeder-Jerde")
+    merchant1.discounts.create!(discount: 20, threshold: 10)
+    merchant1.discounts.create!(discount: 10, threshold: 5)
+    merchant1.discounts.create!(discount: 5, threshold: 5)
+
+    visit "/merchants/#{merchant1.id}/discounts"
+    expect(page).to have_content("Buy 10 items, get 20% off")
+    expect(page).to have_content("Buy 5 items, get 10% off")
+    expect(page).to have_content("Buy 5 items, get 5% off")
+
+    click_link("Delete 5 items, 5% Discount")
+
+    expect(current_path).to eq("/merchants/#{merchant1.id}/discounts")
+
+    expect(page).to have_content("Buy 10 items, get 20% off")
+    expect(page).to have_content("Buy 5 items, get 10% off")
+    expect(page).to_not have_content("Buy 5 items, get 5% off")
+  end
 end
