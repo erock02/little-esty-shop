482ff0e25381da1894b353a8cfbfe82d2eca3ccf
Eric Espindola <eric_e_2012@hotmail.com>
2022-06-14 20:45:34 -0700

    add discount validations to model and tests

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,3 +1,5 @@
 class Discount < ApplicationRecord
   belongs_to :merchant
+
+  validates_presence_of :discount, :threshold
 end
--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -3,4 +3,9 @@ RSpec.describe Discount do
   describe 'relationships' do
     it { should belong_to(:merchant) }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:discount) }
+    it { should validate_presence_of(:threshold) }
+  end
 end
