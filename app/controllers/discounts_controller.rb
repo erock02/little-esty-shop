412211e5f0ae7e9514b3c37e389ee527e9cbe81e
Eric Espindola <eric_e_2012@hotmail.com>
2022-06-14 20:39:34 -0700

    add edit discount feature

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -13,9 +13,24 @@ class DiscountsController < ApplicationController
     @discount = Discount.find(params[:id])
   end
 
+  def edit
+    @discount = Discount.find(params[:id])
+  end
+
   def create
     @merchant.discounts.create!(discount_params)
     redirect_to merchant_discounts_path(@merchant)
+  end
+
+  def update
+    @discount = Discount.find(params[:id])
+    if 0 < params[:discount].to_i && params[:discount].to_i < 100
+      @discount.update(discount_params)
+      redirect_to merchant_discount_path(@merchant, @discount)
+    else
+      flash[:message] = 'Please enter a valid number between 0 and 100 for discount and quantity'
+      redirect_to edit_merchant_discount_path(@merchant, @discount)
+    end
   end
 
   def destroy
