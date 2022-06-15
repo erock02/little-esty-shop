8327e32e7ef427040e6b1656809e48b95c164949
Eric Espindola <eric_e_2012@hotmail.com>
2022-06-15 02:14:33 -0700

    implement feature to see items applied discount

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -7,4 +7,13 @@ class InvoiceItem < ApplicationRecord
   validates :status, presence: true
 
   enum status: {'pending' => 0, 'shipped' => 1, 'packaged' => 2}
+
+  def applied_discount
+    Merchant
+    .joins(:invoice_items, :discounts)
+    .select('discounts.id')
+    .where("invoice_items.id = #{self.id} AND invoice_items.quantity >= discounts.threshold")
+    .order('discounts.discount DESC')
+    .first
+  end
 end
