363a65528cd5e1a1cb1af1215519956ba86a535b
Eric Espindola <eric_e_2012@hotmail.com>
2022-06-14 22:36:54 -0700

    add total revenue including discount feature to invoice show page

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -17,13 +17,23 @@ class Invoice < ApplicationRecord
     invoice_items.sum('invoice_items.unit_price * invoice_items.quantity')
   end
 
+  def total_discounted_revenue
+
+    discount_amount = invoice_items
+    .joins(item: [merchant: :discounts])
+    .where("invoice_items.quantity >= discounts.threshold")
+    .sum('invoice_items.unit_price * invoice_items.quantity * discounts.discount/100')
+
+    total_revenue - discount_amount
+  end
+
   def self.incomplete_invoices
     joins(:invoice_items)
     .where.not(invoice_items: {status: 1})
     .distinct
     .order(:created_at)
   end
-  
+
   def self.invoices_with_merchant_items(merchant)
     merchant.invoices.distinct(:id)
   end
